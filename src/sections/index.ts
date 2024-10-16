4e4cfb42388a1aa04d68e9f2cbb72cafafcc5f7d
Shafiq Khan <mdshafiq.ru@gmail.com>
2024-10-16 10:47:38 +0600

    index file created for sections

---
+++ src/sections/index.ts
@@ -0,0 +1,17 @@
+import { Header } from "./Header";
+import { Hero } from "./Hero";
+import { Features } from "./Features";
+import { LogoTicker } from "./LogoTicker";
+import { Testimonials } from "./Testimonials";
+import { CallToAction } from "./CallToAction";
+import { Footer } from "./Footer";
+
+export {
+  Header,
+  Hero,
+  Features,
+  LogoTicker,
+  Testimonials,
+  CallToAction,
+  Footer,
+}
