b3cf68ab16322b86fb5f7b5ddb707b3538c81238
Shafiq Khan <mdshafiq.ru@gmail.com>
2024-10-16 20:55:24 +0600

    sticky class added to header

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -2,9 +2,9 @@ import { LogoIcon, MenuIcon } from "@/assets/icons";
 import { Button } from "@/components";
 
 export const Header = () => {
-  return <header className='py-4 border-b border-white/20 md:border-none '>
+  return <header className='py-4 border-b border-white/20 md:border-none sticky z-10 '>
   <div className='container '>
-    <div className='flex justify-between items-center md:border border-white/20 md:p-2.5 rounded-xl max-w-2xl mx-auto '>
+    <div className='flex justify-between items-center md:border border-white/20 md:p-2.5 rounded-xl max-w-2xl mx-auto backdrop:blur '>
       <div>
         <div className='border h-10 w-10 rounded-lg inline-flex justify-center items-center border-white/20 '>
           <LogoIcon className='h-8 w-8' />
