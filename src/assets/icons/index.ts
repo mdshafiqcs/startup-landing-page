238a4d3c6e98c76a4c00f4c566b7f26dac0b6b06
Shafiq Khan <mdshafiq.ru@gmail.com>
2024-10-16 09:02:16 +0600

    asset folder structure changed

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,5 @@
+import LogoIcon from "./logo.svg";
+
+export {
+  LogoIcon,
+}
--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,3 +1,15 @@
 export const Header = () => {
-  return <header>Header</header>;
+  return <header className='py-4'>
+    <div className='container'>
+      <div className="flex justify-between">
+        <div>
+          <span>logo</span>
+        </div>
+        <div>
+          <button>Join waitlist</button>
+          <span>menu</span>
+        </div>
+      </div>
+    </div>
+  </header>;
 };
