90710fc8c0401c834518e706222bb26679cc279f
Shafiq Khan <mdshafiq.ru@gmail.com>
2024-10-16 10:51:16 +0600

    updated avatar imports

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -1,7 +1,5 @@
-import avatar1 from "@/assets/avatar-1.png";
-import avatar2 from "@/assets/avatar-2.png";
-import avatar3 from "@/assets/avatar-3.png";
-import avatar4 from "@/assets/avatar-4.png";
+import {avatar1, avatar2, avatar3, avatar4} from "@/assets/images";
+
 
 const testimonials = [
   {
