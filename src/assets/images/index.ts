b7648e5a6b4b4914214870cfc82a9f205c02b006
Shafiq Khan <mdshafiq.ru@gmail.com>
2024-10-16 10:50:54 +0600

    images imported

---
+++ src/assets/images/index.ts
@@ -0,0 +1,13 @@
+import avatar1 from "./avatar-1.png";
+import avatar2 from "./avatar-2.png";
+import avatar3 from "./avatar-3.png";
+import avatar4 from "./avatar-4.png";
+
+
+
+export {
+  avatar1,
+  avatar2,
+  avatar3,
+  avatar4,
+}
