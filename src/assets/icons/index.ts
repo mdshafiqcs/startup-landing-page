45ff7abf6583a165fdfe3e4f639efc62028c42c2
Shafiq Khan <mdshafiq.ru@gmail.com>
2024-10-18 08:52:50 +0600

    icons imported

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,7 +1,14 @@
 import LogoIcon from "./logo.svg";
 import MenuIcon from "./icon-menu.svg";
 
+import SocialX from "./social-x.svg";
+import SocialInsta from "./social-instagram.svg";
+import SocialYoutube from "./social-youtube.svg";
+
 export {
   LogoIcon,
   MenuIcon,
+  SocialX,
+  SocialInsta,
+  SocialYoutube,
 }
